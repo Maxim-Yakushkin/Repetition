c7b5c642a4541f051c6e2259deb849c7fd3c9601
Maxim-Yakushkin <maxigb3320>
2021-01-07 20:26:14 +0300

    18.1 lesson. Generics. Theory-1

--- src/com/yakushkin/repetition/lesson18_1_Generics_Teory/Runner.java
+++ src/com/yakushkin/repetition/lesson18_1_Generics_Teory/Runner.java
@@ -1,5 +1,7 @@
 package com.yakushkin.repetition.lesson18_1_Generics_Teory;
 
+import java.util.ArrayList;
+
 public class Runner {
 
     public static void main(String[] args) {
@@ -7,8 +9,18 @@ public class Runner {
         myList.add("String1");
         myList.add("String2");
         myList.add("String3");
+        myList.add("String4");
+        myList.add("String5");
+        myList.add("String6");
+        myList.add("String7");
+        myList.add("String8");
+        myList.add("String9");
+        myList.add("String10");
 
-        String element = myList.getElement(1);
-        System.out.println(element.toUpperCase());
+        int index = 9;
+        String element = myList.getElement(index);
+        System.out.println("Элемент с индексом " + index + " = " + element.toUpperCase());
+        System.out.println("--------------------------");
+        System.out.println("Размер массива: " + myList.getSize());
     }
 }
