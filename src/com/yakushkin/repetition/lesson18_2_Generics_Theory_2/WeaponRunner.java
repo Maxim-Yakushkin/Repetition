9528741bf4b4d78c368bcc0a6a1a4064ca3f99f3
Maxim-Yakushkin <maxigb3320>
2021-01-09 02:45:27 +0300

    18.2 lesson. Generics. Theory-2

--- src/com/yakushkin/repetition/lesson18_2_Generics_Theory_2/WeaponRunner.java
+++ src/com/yakushkin/repetition/lesson18_2_Generics_Theory_2/WeaponRunner.java
@@ -7,10 +7,19 @@ import com.yakushkin.repetition.lesson18_2_Generics_Theory_2.weapon.Sword;
 
 public class WeaponRunner {
     public static void main(String[] args) {
-        Archer<Bow> legolas = new Archer<>("Legolas", 15);
+        Archer<Bow> legolas = new Archer<>("Legolas", 10);
         legolas.setWeapon(new Bow());
+        Integer damage = legolas.getDamage();
+        int legolasDamage = legolas.getWeapon().getDamage();
+        System.out.println(damage + "\n" + legolasDamage);
 
-        Warrior<Sword> boromir = new Warrior<>("Boromir", 10);
+        System.out.println();
+
+        Warrior<Sword> boromir = new Warrior<>("Boromir", 15);
         boromir.setWeapon(new Sword());
+        Integer damage1 = boromir.getDamage();
+        int boromirDamage = boromir.getWeapon().getDamage();
+
+        System.out.println(damage1 + "\n" +boromirDamage);
     }
 }
