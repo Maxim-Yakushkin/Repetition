4f7f2bfaa71b4ca5eb4edc97e9ea6498cb4e53f5
Maxim <max-yakushkin1@yandex.ru>
2021-01-28 03:43:37 +0300

    Stream. MapReduce

---
+++ src/com/yakushkin/repetition/lesson_21_3_Stream_MapReduce/MapReduceExample.java
@@ -0,0 +1,36 @@
+package com.yakushkin.repetition.lesson_21_3_Stream_MapReduce;
+
+import java.util.stream.Stream;
+
+public class MapReduceExample {
+
+    // Из 3-х миллионов студентов найти студента с максимальным возратсом
+
+    /**
+     * разбиваем на 3 группы и будем паралленльно искать в 3-х группах
+     * <p>
+     * [1_000_000] -> age -> max age
+     * <p>
+     * [1_000_000] -> age -> max age -> max!
+     * <p>
+     * [1_000_000] -> age -> max age
+     */
+    public static void main(String[] args) {
+        Stream.of(
+                new Student(18, "Ivan"),
+                new Student(23, "Petr"),
+                new Student(34, "Vasia"),
+                new Student(45, "Sveta"),
+                new Student(20, "Katya"),
+                new Student(68, "Denis"),
+                new Student(101, "Kira")
+        )
+                .parallel() // распараллеливаем выполение расчетов
+//                .sequential() // целенаправленно используем 1 поток
+                .map(Student::getAge)
+//                .reduce(Math::max)
+//                .reduce(Math::min)
+                .reduce(Integer::sum)       //сумма всех возрастов
+                .ifPresent(System.out::println);
+    }
+}
---
+++ src/com/yakushkin/repetition/lesson_21_3_Stream_MapReduce/Student.java
@@ -0,0 +1,28 @@
+package com.yakushkin.repetition.lesson_21_3_Stream_MapReduce;
+
+public class Student {
+
+    private int age;
+    private String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
