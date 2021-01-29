2ceb609ff263a015b2d034508dbfffcde5d06d91
Maxim <max-yakushkin1@yandex.ru>
2021-01-29 15:49:11 +0300

    Потоки ввода-вывода. File

--- src/com/yakushkin/repetition/lesson_22_1_File/FileRunner.java
+++ src/com/yakushkin/repetition/lesson_22_1_File/FileRunner.java
@@ -5,28 +5,28 @@ import java.io.IOException;
 import java.util.Arrays;
 
 /**
- *              output stream
+ * output stream
  * application ----------------> file
- *
- *              input stream
+ * <p>
+ * input stream
  * application <---------------- file
- *
- * */
+ */
 public class FileRunner {
     public static void main(String[] args) throws IOException {
         File file = new File("resources/test.txt");
-        System.out.println(file.createNewFile());
-        System.out.println(file.exists());
-        System.out.println(file.isFile());
-        System.out.println(file.isDirectory());
-        System.out.println(file.mkdir());
-        System.out.println(file.mkdirs());
-        System.out.println(file.getName());
-        System.out.println(file.getPath());
-        System.out.println(file.getAbsolutePath());
-        System.out.println(file.getParent());
-        System.out.println(file.getAbsoluteFile());
-        System.out.println(file.length());
+        System.out.println("delete: " + file.delete());
+        System.out.println("create: " + file.createNewFile());
+        System.out.println("exist: " + file.exists());
+        System.out.println("isFile: " + file.isFile());
+        System.out.println("isDirectory: " + file.isDirectory());
+        System.out.println("mkdir: " + file.mkdir());
+        System.out.println("mkdirs: " + file.mkdirs());
+        System.out.println("name: " + file.getName());
+        System.out.println("path: " + file.getPath());
+        System.out.println("absolutePath: " + file.getAbsolutePath());
+        System.out.println("parent: " + file.getParent());
+        System.out.println("absoluteFile: " + file.getAbsoluteFile());
+        System.out.println("length: " + file.length());
 
         File dir = new File("resources/test");
         dir.mkdirs();
